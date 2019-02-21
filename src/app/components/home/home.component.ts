998624bf6cc3c5855e7d18c4fab6ed329f534a09
Apol0x <madejapolo@gmail.com>
2019-02-21 14:11:25 +0100

    MOD: introducción a Pipes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { strictEqual } from 'assert';
+import { stringify } from '@angular/core/src/util';
+import { Observable, Subscriber, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public a: number;
   public b: number;
+  public date: Date;
+  public collection: string[];
+  public time;
 
   constructor() { }
 
   ngOnInit() {
     this.a = 0.259;
     this.b = 1.3495;
+    this.date = new Date();
+    this.collection = [] =['a','b','c','d'];
+    this.time = new Observable<string>((observer: Observer<string>) => {
+      setInterval(() => observer.next(new Date().toString()), 1000);
+    });
   }
 
 }
