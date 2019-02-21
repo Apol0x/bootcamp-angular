8ed1537d518c5cec4565ccf00379255a147636ca
Apol0x <madejapolo@gmail.com>
2019-02-21 09:52:36 +0100

    MOD: utilizando las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
+import { FormSearchComponent } from './search/form-search/form-search.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListOwnersComponentComponent } from './search/list-owners-component/list-owners-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ /*  {
+    path: '',
+    component: AppComponent
+  }, */
+  {
+    path: 'search',
+   component: FormSearchComponent
+  },
+  {
+    path: 'owners',
+    component: ListOwnersComponentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
